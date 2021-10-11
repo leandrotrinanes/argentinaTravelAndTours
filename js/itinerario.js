19a5edf1be8272ce1465bc21f2a58e0f194d6140
leandrotrinanes <leandrotrinanes@gmail.com>
2021-10-10 21:52:19 -0300

    Carousel itinerarios

--- js/itinerario.js
+++ js/itinerario.js
@@ -1,7 +1,5 @@
 var itinerarios= [];
 var dia= [];
-/* var cod= 24;
-var lang= "es"; */
 var seleccionar = [];
 
 
@@ -39,9 +37,54 @@ $.ajax({
     /* Titulo */
     imprimir = imprimir + `<h2 class="text-center destinos__paquetes">${itinerarios[cod-1].destinos}</h2>`;
     imprimir = imprimir + `<h3 class="text-center ">${itinerarios[cod-1].dias} dias</h3>`;
-    /* Carrousel de imagenes */
 
-    
+    /* Carrousel de imagenes versión 1*/
+    imprimir = imprimir +
+    `<div id="ItinerariosCarrousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#ItinerariosCarrousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    `;
+
+    for (let i=1; i<itinerarios[cod-1].itinerario.length;i++){
+      imprimir = imprimir + 
+      `<button type="button" data-bs-target="#ItinerariosCarrousel" data-bs-slide-to="${i}" aria-label="Slide ${i+1}"></button>`;
+    }
+
+    imprimir = imprimir + 
+    `
+    </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="${dia[parseInt (itinerarios[cod-1].itinerario[0])-1].url}" class="d-block w-100" alt="buenos aires dia - 1">
+        </div>
+    `;
+
+    for (let i=1; i<itinerarios[cod-1].itinerario.length;i++){
+      imprimir = imprimir + 
+      `
+      <div class="carousel-item">
+        <img src="${dia[parseInt (itinerarios[cod-1].itinerario[i])-1].url}" class="d-block w-100" alt="...">
+      </div>
+      `;
+    }
+
+    imprimir = imprimir + 
+    `
+    <button class="carousel-control-prev" type="button" data-bs-target="#ItinerariosCarrousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#ItinerariosCarrousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    </div>
+    `;
+
+
+
+
     /**Aca armo la lista con collapsable items */
     imprimir = imprimir + `<div class="accordion" id="accordionExample">`;
 
@@ -57,7 +100,7 @@ $.ajax({
               <div id="collapse-${i}" class="accordion-collapse collapse" aria-labelledby="heading-${i}" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                   <div class="row">
-                      <img class="col col-md-6" src="${dia[parseInt (itinerarios[cod-1].itinerario[i-1])-1].url }" alt="buenos aires dia - ${i}">
+                      <img class="col col-md-6" src="${dia[parseInt (itinerarios[cod-1].itinerario[i-1])-1].url}" alt="buenos aires dia - ${i}">
                       <p class="col col-md-6">${dia[parseInt (itinerarios[cod-1].itinerario[i-1])-1].detalle[lang] }</p>
                   </div>
                 </div>
